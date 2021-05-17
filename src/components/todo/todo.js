fa12b5a82c7fc48c438de6f7de2d4978b2d70602
fatensamman <arch.fatensamman@gmail.com>
2021-05-18 01:42:45 +0300

    tryAjax

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,69 +1,68 @@
 import React from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Container, Col, Row, Card } from 'react-bootstrap';
-import axios from 'axios';
 import './todo.scss';
 import useAjax from '../hooks/useAjaxHook.js';
 
 
 function ToDo() {
-  const url='http://localhost:3005/api/v1/todos/';
+  const url = 'https://api-js401.herokuapp.com/api/v1/todo';
   const [list, setList] = useState([]);
-  
+
   useEffect(() => {
     document.title = `To Do List : complete ${list.filter(item => item.complete).length} / incomplete ${list.filter(item => !item.complete).length}`
   })
 
   // useEffect(_getTodoItems, [_getTodoItems]);
 
-    const addItem = (item) => {
-      item.complete = false;
+  const addItem = (item) => {
+    item.complete = false;
 
-      async function _add() {
-          let results = await useAjax({url, body:item, method: 'post'})
-          item._id = results.data._id;
-          // console.log(result,'addItem =---------')
-          setList([...list, item]);
-      }
-      _add();
+    async function _add() {
+      let results = await useAjax({ url, body: item, method: 'post' })
+      item._id = results.data._id;
+      // console.log(result,'addItem =---------')
+      setList([...list, item]);
+    }
+    _add();
   }
 
-    const toggleComplete = id => {
+  const toggleComplete = id => {
 
-      let item = list.filter(i => i._id === id)[0] || {};
+    let item = list.filter(i => i._id === id)[0] || {};
 
-      if (item._id) {
-        item.complete = !item.complete;
-        let newList = list.map(listItem => listItem._id === item._id ? item : listItem);
-        setList(newList);
-      }
-      async function _Complete() {
-        // console.log('this is the PUT req', item)
-        await useAjax({url:`${url}${item._id}`, body:item, method: 'put'});
-        // console.log('this is the result from the PUT', results)
+    if (item._id) {
+      item.complete = !item.complete;
+      let newList = list.map(listItem => listItem._id === item._id ? item : listItem);
+      setList(newList);
+    }
+    async function _Complete() {
+      // console.log('this is the PUT req', item)
+      await useAjax({ url: `${url}${item._id}`, body: item, method: 'put' });
+      // console.log('this is the result from the PUT', results)
     }
     _Complete();
-    };
+  };
 
-    function handleDelete(id){
-      async function _handleDelete(id) {
-           await useAjax({url:url+id, method:'delete'});
-          let newList = list.filter(item => item._id !== id);
-          return setList(newList);
-      }
-      _handleDelete(id);
+  function handleDelete(id) {
+    async function _handleDelete(id) {
+      await useAjax({ url: url + id, method: 'delete' });
+      let newList = list.filter(item => item._id !== id);
+      return setList(newList);
+    }
+    _handleDelete(id);
   }
 
 
   useEffect(() => {
     async function _getData() {
-        // let res = {};
-        let res = await useAjax({url, method: 'get'});
-        console.log('this is the res back from api', res)
-        setList(res.data.results)
+      // let res = {};
+      let res = await useAjax({ url, method: 'get' });
+      console.log('this is the res back from api', res)
+      setList(res.data.results)
     }
     _getData();
   }, []);
@@ -93,8 +92,6 @@ function ToDo() {
               />
             </div>
           </Col>
-
-
         </Row>
       </Container>
 
