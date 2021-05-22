d05ac36fd376a3b2854d88b08fa556047f021d6c
fatensamman <arch.fatensamman@gmail.com>
2021-05-22 21:30:01 +0300

    lab34

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginProvider from './components/auth/context.js';
 
 import App from './App.js';
 
-function Main () {
-  return <App />;
+function Main() {
+  return(
+  <LoginProvider>
+    <App />;
+  </LoginProvider>
+  )
 }
 
 const rootElement = document.getElementById('root');
